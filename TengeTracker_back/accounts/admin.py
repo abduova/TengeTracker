d8cd2f727c61441e6cf53d5d5ba6da5d83292332
abduova <a_abduova@kbtu.kz>
2026-04-19 02:44:34 +0500

    fixed categories naming

--- TengeTracker_back/accounts/admin.py
+++ TengeTracker_back/accounts/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Wallet
+from .models import Wallet, Category
 
 admin.site.register(Wallet)
+admin.site.register(Category)
--- TengeTracker_back/accounts/models.py
+++ TengeTracker_back/accounts/models.py
@@ -9,4 +9,19 @@ class Wallet(models.Model):
 
     def __str__(self):
         return self.name
-    
+
+class Category(models.Model):
+    TYPE_CHOICES = (
+        ('income', 'Income'),
+        ('expense', 'Expense'),
+    )
+
+    name = models.CharField(max_length=100)
+    type = models.CharField(max_length=10, choices=TYPE_CHOICES)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
