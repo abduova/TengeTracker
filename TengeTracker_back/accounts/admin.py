ee59967bd2cdd5674ba80e98c1d91af2dfda9574
abduova <a_abduova@kbtu.kz>
2026-04-21 12:51:20 +0500

    added Tag model (4th model) and updated admin

--- TengeTracker_back/accounts/admin.py
+++ TengeTracker_back/accounts/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-
 # Register your models here.
-from .models import Wallet, Category, Transaction
+from .models import Wallet, Category, Transaction, Tag
 
 admin.site.register(Wallet)
 admin.site.register(Category)
 admin.site.register(Transaction)
+admin.site.register(Tag)
--- TengeTracker_back/accounts/models.py
+++ TengeTracker_back/accounts/models.py
@@ -58,3 +58,11 @@ class Transaction(models.Model):
 
     def __str__(self):
         return f"{self.amount} - {self.category.name}"
+    
+    
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
